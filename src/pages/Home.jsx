60cddd561d175f53465e595f3e6dec3b6a16a4be
devops4sure <devops4sure@gmail.com>
2026-07-16 18:15:47 +0530

    hh

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <ManufacturingGallery />
       <ApplicationsSection />
       <ClientsStrip />
-      <GlobalReachSection />
+      {/* <GlobalReachSection /> */}
       <ContactCta />
     </>
   );
